01442de7f9805fac28197ab01f6f10fb59c8c103
Marouan <145381165+xe-rx@users.noreply.github.com>
2026-01-21 13:42:05 +0100

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,29 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "c", "rust", "vim", "vimdoc", "html", "css", "cpp", "javascript", "typescript"},
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-      additional_vim_regex_highlighting = false,
-		})
-	end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "c",
+        "cpp",
+        "rust",
+        "vim",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
