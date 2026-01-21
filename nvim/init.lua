1eaee7e447273251ef78865c25bde18086e5e1a9
Marouan <145381165+xe-rx@users.noreply.github.com>
2026-01-21 13:47:07 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,35 +1,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-vim.fn.system({
-"git",
-"clone",
-"--filter=blob:none",
-"https://github.com/folke/lazy.nvim.git",
-"--branch=stable",
-lazypath,
-})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
+
 require("vim-binds")
+
 vim.opt.clipboard = "unnamedplus"
-
 vim.opt.number = true
-
 vim.opt.colorcolumn = "80"
+
+-- ✅ Lazy: SETUP ONCE. This loads all plugin specs in lua/plugins/*.lua
 require("lazy").setup("plugins")
+
+-- Keymap
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 
-require("lazy").setup({
-  { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-})
-require("lazy").setup({
-    "plugins",
-    "catppuccin/nvim", -- Add Catppuccin theme
-    name = "catppuccin"
-})
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-
--- show pylint diagnostics inline
+-- Diagnostics
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
@@ -38,27 +32,26 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 
--- Load the Catppuccin theme
-vim.cmd.colorscheme "catppuccin"
+-- If you want catppuccin config here, KEEP it here,
+-- but make sure catppuccin is installed via lua/plugins/catppuccin.lua
+vim.cmd.colorscheme("catppuccin")
 
--- Optional: Set Catppuccin flavor
 require("catppuccin").setup({
-    flavor = "mocha",
-    transparent_background = false,
-    term_colors = true,
-    integrations = {
-        alpha = true,
-        nvimtree = true,
-        telescope = true,
-    },
+  flavor = "mocha",
+  transparent_background = false,
+  term_colors = true,
+  integrations = {
+    alpha = true,
+    nvimtree = true,
+    telescope = true,
+  },
 })
 
+-- Alpha dashboard setup (unchanged)
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
--- Set header
 dashboard.section.header.val = {
-
                 "",
                 "",
                 "      ░░                                                  ░░      ",
@@ -104,85 +97,53 @@ dashboard.section.header.val = {
                 "            ██    ██    ████░░░░░░░░░░████    ██▓▓▓▓██            ",
                 "          ████████          ████░░████          ████████          ",
                 "                                ██                                "
-
 }
 
--- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+  dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+  dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+  dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+  dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 }
 
--- Set footer
---   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
---   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
---   ```init.lua
---   return require('packer').startup(function()
---       use 'wbthomason/packer.nvim'
---       use {
---           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
---           requires = {'BlakeJC94/alpha-nvim-fortune'},
---           config = function() require("config.alpha") end
---       }
---   end)
---   ```
--- local fortune = require("alpha.fortune")
--- dashboard.section.footer.val = fortune()
-
--- Send config to alpha
 alpha.setup(dashboard.opts)
 
--- Remap Caps Lock to Escape in Normal mode
-vim.api.nvim_set_keymap('n', '<CapsLock>', '<Esc>', { noremap = true })
-
--- Remap Caps Lock to Escape in Insert mode
-vim.api.nvim_set_keymap('i', '<CapsLock>', '<Esc>', { noremap = true })
-
--- Remap Caps Lock to Escape in Visual mode
-vim.api.nvim_set_keymap('v', '<CapsLock>', '<Esc>', { noremap = true })
-
--- remap ESC (exit) to j-k
-vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true, silent = true })
+-- Remaps
+vim.api.nvim_set_keymap("n", "<CapsLock>", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<CapsLock>", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("v", "<CapsLock>", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("i", "jk", "<esc>", { noremap = true, silent = true })
 
 -- Disable folding on alpha buffer
 vim.cmd([[
-    autocmd FileType alpha setlocal nofoldenable
+  autocmd FileType alpha setlocal nofoldenable
 ]])
 
--- Define the highlight group for ExtraWhitespace
+-- Whitespace highlighting
 vim.cmd([[
-    highlight ExtraWhitespace ctermbg=LightGreen guibg=LightGreen
+  highlight ExtraWhitespace ctermbg=LightGreen guibg=LightGreen
 ]])
 
--- Function to set or clear the ExtraWhitespace highlight
 local function set_whitespace_highlighting()
-    -- Check if the current buffer is the dashboard or start screen
-    local bufname = vim.fn.bufname()
-    if bufname == '' or vim.bo.filetype == 'dashboard' then
-        -- Clear the highlight if it's the dashboard or start screen
-        vim.cmd('match none')
-    else
-        -- Set the highlight otherwise
-        vim.cmd('match ExtraWhitespace /\\s\\+$/')
-    end
+  local bufname = vim.fn.bufname()
+  if bufname == "" or vim.bo.filetype == "dashboard" then
+    vim.cmd("match none")
+  else
+    vim.cmd("match ExtraWhitespace /\\s\\+$/")
+  end
 end
 
--- for pynvim integration ig? automatically laods virtual env
-vim.g.python3_host_prog = '/home/xerx/myenv/bin/python'
+-- Python host (keep if correct for your machine)
+vim.g.python3_host_prog = "/home/xerx/myenv/bin/python"
 
--- Set the initial highlighting based on the current buffer
 set_whitespace_highlighting()
 
--- Update the highlighting whenever the buffer changes
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-    callback = set_whitespace_highlighting
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  callback = set_whitespace_highlighting,
 })
 
--- Automatically remove trailing whitespace on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    command = "%s/\\s\\+$//e"
+  pattern = "*",
+  command = "%s/\\s\\+$//e",
 })
