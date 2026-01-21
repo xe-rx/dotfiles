b3f47ba2ad80180a2fc02716fb7f6e47bfe0e4be
Marouan <145381165+xe-rx@users.noreply.github.com>
2026-01-21 14:11:11 +0100

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,42 +1,19 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    lazy = false, -- required: this plugin does not support lazy-loading
+    branch = "master",
     build = ":TSUpdate",
     config = function()
-      local ts = require("nvim-treesitter")
-
-      -- basic setup (new API)
-      ts.setup({
-        install_dir = vim.fn.stdpath("data") .. "/site",
-      })
-
-      -- install parsers (sync on startup; remove :wait() if you prefer async)
-      ts.install({
-        "lua",
-        "c",
-        "cpp",
-        "rust",
-        "vim",
-        "vimdoc",
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-      }):wait(300000)
-
-      -- enable highlighting automatically when a buffer's filetype is set
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          pcall(vim.treesitter.start)
-        end,
-      })
-
-      -- enable treesitter indentation (optional)
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua","c","cpp","rust","vim","vimdoc","html","css","javascript","typescript",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
       })
     end,
   },
